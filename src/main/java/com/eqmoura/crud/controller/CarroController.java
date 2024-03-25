d1ecdfba55cfdc5e64d99ac4e882732c606cbc6f
VICTOR LEANDRO VIEIRA MORA <victor.mora1@alunos.sc.senac.br>
2024-03-25 20:02:57 -0300

    atualizado o template do thymeleaf para exibir o carro que foi inserido no database

--- src/main/java/com/eqmoura/crud/controller/CarroController.java
+++ src/main/java/com/eqmoura/crud/controller/CarroController.java
@@ -2,6 +2,7 @@ package com.eqmoura.crud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,11 +17,13 @@ public class CarroController {
 
     @PostMapping("/entrada")
     public String salvarCarro(@RequestParam("modelo") String modelo, @RequestParam("fabricante") String fabricante,
-            @RequestParam("placa") String placa) {
+            @RequestParam("placa") String placa, Model m) {
 
         Carro carro = new Carro(modelo, fabricante, placa, null);
 
         carroRepository.save(carro);
+
+        m.addAttribute("carro", carro);
 
         return "sucesso_carro";
     }
